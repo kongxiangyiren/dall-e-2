5d4d1414e16e56e0b63a6467b581fc76e7ddd441
空巷一人 <1530688385@qq.com>
2024-09-04 16:56:16 +0800

    修改mac打包错误

--- build.exe.js
+++ build.exe.js
@@ -8,9 +8,18 @@ rmSync(`./${runName}`, {
   force: true,
 });
 cpSync(process.execPath, runName);
+
+if (process.platform === 'darwin') {
+  execSync(`codesign --remove-signature ${runName}`);
+}
+
 execSync(
-  `npx postject ${runName} NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2`,
+  `npx postject ${runName} NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 ${process.platform === 'darwin' ? '--macho-segment-name NODE_SEA' : ''}`,
 );
+
+if (process.platform === 'darwin') {
+  execSync(`codesign --sign - ${runName}`);
+}
 
 rmSync(`./dist`, {
   recursive: true,
